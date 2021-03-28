1e26b50b2fccb4914f661bd302e65c3dbc18befd
prasunmax <prasunmax@gmail.com>
2021-03-28 12:34:40 +0530

    Fixed all issues

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,1 +1,76 @@
 const Posts = require('../models/posts');
+const { Op } = require('sequelize');
+
+module.exports = {
+    post_api: post_handler,
+    get_api: get_handler
+}
+
+
+function post_handler(req, res) {
+    let tmpdata = req.body;
+    if (tmpdata.isPublished) {
+        tmpdata.publishedDate = new Date().getTime();
+    }
+    Posts.create(tmpdata)
+        .then(data => {
+            res.status(201).send(data);
+        }).catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while creating the Reminder."
+            });
+        });
+};
+
+function get_handler(req, res) {
+    const id = req.params.id;
+    if (id) {
+        Posts.findByPk(id)
+            .then(data => {
+                if (data)
+                    res.status(200).send(data);
+                else
+                    res.status(404).send("ID not found");
+            })
+            .catch(err => {
+                res.status(404).send({
+                    message: "ID not found"
+                });
+            });
+    } else {
+        const author = req.query.author;
+        try {
+            var hasPublished = req.query.isPublished && (req.query.isPublished === 'true' || req.query.isPublished === 'false') ? true : false;
+            var isPublished = (req.query.isPublished && req.query.isPublished === 'true') ? true : false;
+        } catch (err) { };
+        var hasqry = false;
+
+        var jsonData = {};
+        var whereJson = {};
+
+        if (author) {
+            jsonData['author'] = author;
+            hasqry = true;
+        }
+        if (hasPublished) {
+            jsonData['isPublished'] = isPublished;
+            hasqry = true;
+        }
+        if (hasqry) {
+            //qryStr = '{ "where": { ' + qryStr + ' } }';
+            whereJson['where'] = jsonData;
+        } else {
+            qryStr = '{}';
+        }
+        Posts.findAll(whereJson)
+            .then(data => {
+                res.send(data);
+            })
+            .catch(err => {
+                res.status(404).send({
+                    message: "No Records Found"
+                });
+            });
+    }
+};
--- routes/posts.js
+++ routes/posts.js
@@ -2,4 +2,18 @@ const express = require('express');
 const router = express.Router();
 const postsController = require('../controllers/posts');
 
+
+router.get('/', postsController.get_api);
+router.get('/:id', postsController.get_api);
+router.post('/', postsController.post_api);
+router.patch('/:id', function (req, res, next) {
+    res.status(405).send('<p>API does not allow deleting or modifying reminders for any id value</p>');
+});
+router.put('/:id', function (req, res, next) {
+    res.status(405).send('<p>API does not allow deleting or modifying reminders for any id value</p>');
+});
+router.delete('/:id', function (req, res, next) {
+    res.status(405).send('<p>API does not allow deleting or modifying reminders for any id value</p>');
+});
+
 module.exports = router;
